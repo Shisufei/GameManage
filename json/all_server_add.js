e3b96b0e57950c9670f368522daae6b39655c467
Shisufei <656819902@163.com>
2018-04-02 15:09:28 +0800

    first commit

---
+++ json/all_server_add.js
@@ -0,0 +1,23 @@
+{
+    "code": 0,
+    "msg": "",
+    "count": 16,
+    "data": [
+        {"date":"2018-03-20","dayAdd":1100,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-21","dayAdd":1500,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-22","dayAdd":1200,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-23","dayAdd":1300,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-24","dayAdd":1800,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-25","dayAdd":2000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-26","dayAdd":1000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-27","dayAdd":1000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-28","dayAdd":1000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-29","dayAdd":1000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-11","dayAdd":1000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-12","dayAdd":1000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-13","dayAdd":1000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-14","dayAdd":1000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-15","dayAdd":1000,"payerAdd":200,"totalAdd":1500},
+        {"date":"2018-03-16","dayAdd":1000,"payerAdd":200,"totalAdd":1500}
+    ]
+}
