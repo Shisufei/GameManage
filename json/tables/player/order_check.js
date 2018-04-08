db35c5dd311579ebb3eced7eb1c19af93cdbb4d7
Shisufei <656819902@163.com>
2018-04-08 17:50:28 +0800

    add some tables

---
+++ json/tables/player/order_check.js
@@ -0,0 +1,12 @@
+{
+    "code": 0,
+    "msg": "",
+    "count": 5,
+    "data": [
+    {"addtime":"2018-03-20","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyType":"我是补单类型","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注"},
+    {"addtime":"2018-03-21","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyType":"我是补单类型","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注"},
+    {"addtime":"2018-03-22","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyType":"我是补单类型","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注"},
+    {"addtime":"2018-03-23","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyType":"我是补单类型","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注"},
+    {"addtime":"2018-03-24","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyType":"我是补单类型","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注"}
+]
+}
---
+++ json/tables/player/order_stream.js
@@ -0,0 +1,12 @@
+{
+    "code": 0,
+    "msg": "",
+    "count": 5,
+    "data": [
+    {"addtime":"2018-03-20","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注","status":"待审核","operateRemark":"我是审核备注","operateTime":"2018-04-10 09:00:00"},
+    {"addtime":"2018-03-21","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注","status":"待审核","operateRemark":"我是审核备注","operateTime":"2018-04-10 09:00:00"},
+    {"addtime":"2018-03-22","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注","status":"待审核","operateRemark":"我是审核备注","operateTime":"2018-04-10 09:00:00"},
+    {"addtime":"2018-03-23","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注","status":"待审核","operateRemark":"我是审核备注","operateTime":"2018-04-10 09:00:00"},
+    {"addtime":"2018-03-24","no":"A1","nickname":"哈哈哈","server":"安卓1区","applyContent":"我是补单内容","num":10,"applyRemark":"我是补单备注","status":"待审核","operateRemark":"我是审核备注","operateTime":"2018-04-10 09:00:00"}
+]
+}
